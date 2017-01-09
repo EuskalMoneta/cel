f98baf11e70dfd29a1ff1e1acfc80c22a2553430
Florian Le Frioux <florian.lefrioux@meta-it.fr>
2017-01-09 15:09:34 +0100

    First commit for Compte en Ligne

---
+++ src/cel/cel/urls.py
@@ -0,0 +1,42 @@
+"""gi URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+
+from django.conf.urls import url
+from django.contrib.auth.views import logout
+from django.core.urlresolvers import reverse_lazy
+
+from adherents import views as adherents_views
+from base import views as base_views
+from cel.auth import login_view
+
+urlpatterns = [
+    # built-in Django i18n:
+    # from django.conf.urls import include, i18n
+    # url(r'^i18n/', include(i18n)),
+    url(r'^i18n/setlang_custom/$', base_views.setlang_custom, name='setlang_custom'),
+
+    # JavaScript config for this Django/React app
+    url(r'^config\.js$', base_views.config_js, name='config_js'),
+    # login
+    url(r'^login/?$', login_view, name='login'),
+    # logout
+    url(r'^logout/?$', logout, {'next_page': reverse_lazy('home')}, name='logout'),
+    # change-password
+    url(r'^change-password/?$', base_views.change_password, name='change-password'),
+
+    # home
+    url(r'^$', adherents_views.index, name='home'),
+]
---
+++ src/cel/webpack.config.js
@@ -0,0 +1,140 @@
+var webpack = require('webpack')
+var path = require('path')
+// Plugin Webpack pour la traduction
+var I18nPlugin = require("i18n-webpack-plugin")
+
+// Liste des langues disponibles dans notre application
+var languages = {
+    "fr": null,
+    "eu": require('./static/locales/eu.json')  // Fichier de traduction
+}
+
+module.exports = Object.keys(languages).map(function(language) {
+    return {
+        // the base directory (absolute path) for resolving the entry option
+        context: __dirname,
+
+        // If you have a big problem on the front-side (with React, etc ...)
+        // You can activate this, its generates the .map for the bundles,
+        // and using Chrome this helps a LOT to locate where the problem is in your code exactly:
+        // ^^^               ^^^
+        // devtool: 'source-map',
+
+        // Liste des pages/scripts React qui composent notre application,
+        // Pour chaque fichier .jsx servant nos pages, nous devons le déclarer ici !
+        // A noter l'exception des Utils et autres dépendances que nous codons et importons nous-même
+        // Voir la section sur les alias dans ce fichier de config
+        entry: {
+            Base: './static/js/base',
+            ChangePassword: './static/js/change-password',
+            Login: './static/js/login',
+        },
+
+        // Où vont se situer le résultat de la compilation effectuée par Webpack (nos bundles utilisés par notre navigateur)
+        // Ici: /assets/bundles/js/<langue>.<nom_script>.js
+        output: {
+            // where you want your compiled bundle to be stored
+            path: '/assets/bundles/',
+            // naming convention webpack should use for your files
+            filename: 'js/'+ language + '.[name].js',
+        },
+
+        // Modules externes utilisés par nos pages/scripts React
+        plugins: [
+            // makes our dependencies available in every module
+            new webpack.ProvidePlugin({
+                Promise: 'imports?this=>global!exports?global.Promise!es6-promise',
+                fetch: 'imports?this=>global!exports?global.fetch!whatwg-fetch',
+                Raven: 'raven-js',
+                React: 'react',
+                ReactDOM: 'react-dom',
+                ReactToastr: 'react-toastr',
+                Formsy: 'formsy-react',
+                FRC: 'formsy-react-components',
+                moment: 'moment',
+                "_": 'underscore'
+            }),
+            new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /fr|eu/),
+            new I18nPlugin(
+                languages[language]
+            )
+        ],
+
+        // Les modules permettent à Webpack de charger d'autres types de données que le JavaScript (JSX dans notre cas)
+        // Babel est le nom du compilateur utilisé par Webpack pour compiler notre JSX
+        // Webpack permet également de charger nos feuilles de styles (CSS), entre autres polices, SVG, etc...
+        module: {
+            loaders: [
+                // a regexp that tells webpack use the following loaders on all
+                // .js and .jsx files
+                {
+                    test: /\.jsx?$/,
+                    // we definitely don't want babel to transpile all the files in
+                    // node_modules. That would take a long time.
+                    exclude: /node_modules/,
+                    // use the babel loader
+                    loader: 'babel',
+                    query: {
+                        "presets": ["es2015", "stage-0", "react"]
+                    }
+                },
+                // Classic CSS + SASS preprocessor
+                {
+                    test: /\.css$/,
+                    exclude: /\.useable\.css$/,
+                    loaders: ['style', 'css']
+                },
+                {
+                    test: /\.useable\.css$/,
+                    loaders: ['style/useable', 'css']
+                },
+                {
+                    test: /\.scss$/,
+                    loaders: ["style", "css?sourceMap", "sass?sourceMap"]
+                },
+                {
+                    test: /\.json/,
+                    loader: 'json-loader'
+                },
+                // We want to use bootstrap
+                // Bootstrap is based on webfonts / svg and other cool things
+                // We need webpack to handle those for us
+                {
+                    test: /\.svg/,
+                    loader: 'svg-url-loader'
+                },
+                {
+                    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                    loader: "url-loader?limit=10000&mimetype=application/font-woff"
+                },
+                {
+                    test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                    loader: "file-loader"
+                }
+            ]
+        },
+        // Url-loader permet de charger des petits fichiers sous forme Base64 (comme les images, par exemple)
+        // Ce paramètre est la limite de taille de ces fichiers...
+        // Car nous ne voulons pas que nos bundles JS soit trop gros à l'arrivée
+        url: {
+            dataUrlLimit: 1024 // 1 kB
+        },
+
+        // Cette partie de la config, explique à Webpack la manière avec laquelle il va devoir trouver nos fichiers
+        resolve: {
+            root: path.resolve(__dirname),
+
+            // Ceci est différent de la partie sur la liste des pages/scripts React qui composent notre application
+            // Les alias sont les dépendances que nous écrivons pour notre propre code
+            // Que nous pourrons importer depuis celui-ci (comme les Utils ou une modale utilisés un peu partout)
+            alias: {
+                Utils: 'static/js/utils',
+            },
+
+            // tells webpack where to look for modules
+            modulesDirectories: ['node_modules'],
+            // extensions that should be used to resolve modules
+            extensions: ['', '.js', '.jsx']
+        }
+    }
+})

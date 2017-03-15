3ba1d0f3ad431a12e5f44103df23723aa9c758ba
Florian Le Frioux <florian.lefrioux@meta-it.fr>
2017-03-15 17:01:36 +0100

    [IMP] Suite au souci de décimale dans les montants: modification des affichages des montants dans la table Historique + Clean HTML/JS/CSS

--- src/cel/static/js/history.jsx
+++ src/cel/static/js/history.jsx
@@ -19,9 +19,6 @@ import 'node_modules/react-bootstrap-table/dist/react-bootstrap-table.min.css'
 
 const {
     Input,
-    RadioGroup,
-    Row,
-    Textarea,
 } = FRC
 
 import DatePicker from 'react-datepicker'
@@ -147,33 +144,20 @@ var HistoryPage = React.createClass({
                 }
             }
         }
-
-        // var res = _.each(historyList[0].result.pageItems, (item, index, list) => {
-        // }, this.state.historyListWithSolde)
-
         this.setState({historyList: historyList[0].result.pageItems});
     },
 
     componentDidMount() {
-        var computeHistoryData = (data) => {
-            this.setState({allAccount: data.result});
-            this.setState({currentSolde: data.result[0]}, this.refreshTable);
-        }
-        // Get account summary
-        var urlSummary = getAPIBaseURL + "account-summary-adherents/"
-        fetchAuth(urlSummary, 'get', computeHistoryData)
-
         var computeAccountList = (data) => {
             var res = _.chain(data.result)
                 .map(function(item){ return {label: item.number, value:item.status.accountId} })
                 .sortBy(function(item){ return item.label })
                 .value()
-            this.setState({accountList: res})
-            this.setState({currentAccount:res[0]})
+
+            this.setState({currentAccount:res[0], accountList: res,
+                           allAccount: data.result, currentSolde: data.result[0]}, this.refreshTable)
         }
-        fetchAuth(urlSummary, 'GET', computeAccountList)
-
-
+        fetchAuth(getAPIBaseURL + "account-summary-adherents/", 'GET', computeAccountList)
     },
 
     refreshTable() {
@@ -187,7 +171,7 @@ var HistoryPage = React.createClass({
         if (date <= this.state.endDate)
         {
             this.setState({beginDate: date,
-                           selectedValue:  {
+                           selectedValue: {
                                 label: "Autres",
                                 value: "custom"
                            }
@@ -195,7 +179,7 @@ var HistoryPage = React.createClass({
         }
         else {
             this.refs.container.error(
-                __("Attention, la date de début doit être antérieur à celle de fin !"),
+                __("Attention, la date de début doit être antérieure à celle de fin !"),
                 "",
                 {
                     timeOut: 5000,
@@ -209,7 +193,7 @@ var HistoryPage = React.createClass({
     endDateChange(date) {
         if (date >= this.state.beginDate) {
             this.setState({endDate: date,
-                           selectedValue:  {
+                           selectedValue: {
                                 label: "Autres",
                                 value: "custom"
                           }
@@ -217,7 +201,7 @@ var HistoryPage = React.createClass({
         }
         else {
             this.refs.container.error(
-                __("Attention, la date de fin doit être postérieur à celle de début !"),
+                __("Attention, la date de fin doit être postérieure à celle de début !"),
                 "",
                 {
                     timeOut: 5000,
@@ -267,17 +251,11 @@ var HistoryPage = React.createClass({
     },
 
     render() {
-        const options = {
-            noDataText: __("Pas d'historique à afficher."), 
-            hideSizePerPage: true, 
-            sizePerPage: 20,
-        };
-
         // Display current solde information
         if (this.state.currentSolde || this.state.currentSolde === 0) {
             var currentSoldeLabel = (
                 <span className="solde-history-span">
-                    {this.state.currentSolde.status.balance + " " + this.state.currentSolde.currency.suffix}
+                    {this.state.currentSolde.status.balance.replace('.', ',') + " " + this.state.currentSolde.currency.suffix}
                 </span>
             )
         }
@@ -313,19 +291,18 @@ var HistoryPage = React.createClass({
                             ref="select"
                             theme="bootstrap3"
                             onValueChange={this.currentAccountOnValueChange}
-                            value = {this.state.currentAccount}
+                            value={this.state.currentAccount}
                             options={this.state.accountList}
                             renderValue={SelectizeUtils.selectizeRenderValueLineBreak}
                             renderOption={SelectizeUtils.selectizeNewRenderOption}
                             required
                         >
-                            <option value = "day">{this.state.allAccount[0].id}</option>
+                            <option value="day">{this.state.allAccount[0].id}</option>
                         </SimpleSelect>
                     </div>
                 )
             }
         }
-
 
         // History data table
         var dateFormatter = (cell, row) => {
@@ -337,22 +314,30 @@ var HistoryPage = React.createClass({
         var debitFormatter = (cell, row) => {
             // Cell is a string for now,
             // we need to cast it in a Number object to use the toFixed method.
-            if (cell < 0)
-            {
-                return Number(cell).toFixed(2)
+            if (cell < 0) {
+                return Number(cell).toFixed(2).replace('.', ',')
             }
         }
+
         var creditFormatter = (cell, row) => {
             // Cell is a string for now,
             // we need to cast it in a Number object to use the toFixed method.
-            if (cell > 0)
-            {
-                return Number(cell).toFixed(2)
+            if (cell > 0) {
+                return Number(cell).toFixed(2).replace('.', ',')
             }
         }
 
-        var historyTable = (
+        var soldeFormatter = (cell, row) => {
+            return cell.replace('.', ',')
+        }
 
+        const options = {
+            noDataText: __("Pas d'historique à afficher."), 
+            hideSizePerPage: true, 
+            sizePerPage: 20,
+        }
+
+        var historyTable = (
             <BootstrapTable
              data={this.state.historyList} striped={true} hover={true} pagination={true}
              selectRow={{mode: 'none'}} tableContainerClass="react-bs-table-account-history"
@@ -362,101 +347,101 @@ var HistoryPage = React.createClass({
                 <TableHeaderColumn columnClassName="line-break" dataField="description">{__("Libellé")}</TableHeaderColumn>
                 <TableHeaderColumn dataField="amount" dataFormat={debitFormatter}>{__("Débit")}</TableHeaderColumn>
                 <TableHeaderColumn dataField="amount" dataFormat={creditFormatter}>{__("Crédit")}</TableHeaderColumn>
-                <TableHeaderColumn dataField="solde">{__("Solde")}</TableHeaderColumn>
+                <TableHeaderColumn dataField="solde" dataFormat={soldeFormatter}>{__("Solde")}</TableHeaderColumn>
             </BootstrapTable>
         )
 
         return (
             <div className="row">
                 <div className="col-md-10">
-                    <div className="form-group row">
-                        <div className="col-sm-1"></div>
-                        <div className="col-sm-3" style={{paddingLeft: 25}}>{accountData}</div>
-                    </div>
-                    <div className="form-group row">
-                        <div className="col-sm-1"></div>
-                        <div className="col-sm-3" style={{paddingLeft: 25}}>{balanceData}</div>
-                    </div>
-                    <div className="col-md-8 col-md-offset-1">
-                        <div className="search-group" style={{borderRadius: 5, paddingBottom: 10}}>
-                            <div className="form-group row">
-                                <div className="col-sm-4 col-md-offset-1">
-                                    <h4>Rechercher des opérations</h4>
+                    <HistoricalForm ref="historical-form">
+                        <div className="form-group row" style={{marginBottom: 5}}>
+                            <div className="col-sm-3 col-sm-offset-1" style={{paddingLeft: 25}}>{accountData}</div>
+                        </div>
+                        <div className="form-group row">
+                            <div className="col-sm-3 col-sm-offset-1" style={{paddingLeft: 25}}>{balanceData}</div>
+                        </div>
+                        <div className="col-md-8 col-md-offset-1">
+                            <div className="search-group" style={{borderRadius: 5, paddingBottom: 10}}>
+                                <div className="form-group row">
+                                    <div className="col-sm-4 col-md-offset-1">
+                                        <h4>Rechercher des opérations</h4>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="form-group row col-md-offset-1" style={{marginBottom: 0}}>
-                                <label
-                                    className="control-label col-sm-1"
-                                    htmlFor="memberhistorical-description"
-                                    style={{paddingTop:10}}>
-                                    {__("Description")}
-                                </label>
-                                <div className="col-sm-3">
-                                    <HistoricalForm ref="historical-form">
+                                <div className="form-group row col-md-offset-1" style={{marginBottom: 0}}>
+                                    <label
+                                        className="control-label col-sm-1 col-sm-offset-1"
+                                        htmlFor="memberhistorical-description"
+                                        style={{paddingTop:10, marginRight: 10}}>
+                                        {__("Description")}
+                                    </label>
+                                    <div className="col-sm-3">
                                         <Input
                                             name="description"
                                             data-eusko="memberhistorical-description"
                                             onChange={this.descriptionOnValueChange}
-                                            value = {this.state.description}
+                                            value={this.state.description}
+                                            layout="elementOnly"
                                         />
-                                    </HistoricalForm>
+                                    </div>
+                                    <label
+                                        className="control-label col-sm-2"
+                                        htmlFor="memberhistorical-date-period"
+                                        style={{paddingTop:10}}>
+                                        {__("Période")}
+                                    </label>
+                                    <div className="col-sm-4 memberhistorical" data-eusko="memberhistorical-date-period">
+                                        <SimpleSelect
+                                            ref="select"
+                                            placeholder={__("Période")}
+                                            theme="bootstrap3"
+                                            onValueChange={this.dateOnValueChange}
+                                            value={this.state.selectedValue}
+                                            renderResetButton={() => { return null }}
+                                            required
+                                        >
+                                            <option value="day">Aujourd'hui</option>
+                                            <option value="this_week">La dernière semaine</option>
+                                            <option value="last_month">Le dernier mois</option>
+                                            <option value="3_month">Les 3 derniers mois</option>
+                                            <option value="12_month">Les 12 derniers mois</option>
+                                            <option value="custom">Autres</option>
+                                        </SimpleSelect>
+                                    </div>
                                 </div>
-                                <label
-                                    className="control-label col-sm-2"
-                                    htmlFor="memberhistorical-date-period"
-                                    style={{paddingTop:10}}>
-                                    {__("Période")}
-                                </label>
-                                <div className="col-sm-4 memberhistorical" data-eusko="memberhistorical-date-period">
-                                    <SimpleSelect
-                                        ref="select"
-                                        placeholder={__("Période")}
-                                        theme="bootstrap3"
-                                        onValueChange={this.dateOnValueChange}
-                                        value = {this.state.selectedValue}
-                                        renderResetButton={() => { return null }}
-                                        required
-                                    >
-                                        <option value = "day">Aujourd'hui</option>
-                                        <option value = "this_week">La dernière semaine</option>
-                                        <option value = "last_month">Le dernier mois</option>
-                                        <option value = "3_month">Les 3 derniers mois</option>
-                                        <option value = "12_month">Les 12 derniers mois</option>
-                                        <option value = "custom">Autres</option>
-                                    </SimpleSelect>
+                                <div className="form-group row col-md-offset-1" style={{marginBottom:0, marginTop:10}}>
+                                    <label className="control-label col-sm-2 col-md-offset-5" 
+                                           htmlFor="memberhistorical-date-start-end" 
+                                           style={{paddingTop:10}}>
+                                                Date début / fin
+                                    </label>
+                                    <div className="col-sm-2 memberhistorical"
+                                         data-eusko="memberhistorical-date-start"
+                                         style={{marginLeft: 10}}>
+                                        <DatePicker
+                                            name="dateSelectorBegin"
+                                            className="form-control"
+                                            selected={moment(this.state.beginDate)}
+                                            onChange={this.beginDateChange}
+                                            showYearDropdown 
+                                            locale="fr"
+                                        />
+                                    </div>
+                                    <div className="col-sm-2 memberhistorical" data-eusko="memberhistorical-date-end">
+                                        <DatePicker
+                                            name="dateSelectorEnd"
+                                            className="form-control"
+                                            selected={moment(this.state.endDate)}
+                                            onChange={this.endDateChange}
+                                            showYearDropdown 
+                                            locale="fr"
+                                        />
+                                    </div>
+                                    <div className="col-sm-3"></div>
                                 </div>
-                                <div className="col-sm-2"></div>
-                            </div>
-                            <div className="form-group row col-md-offset-1" style={{marginBottom: 0}}>
-                                <label className="control-label col-sm-2 col-md-offset-4" 
-                                       htmlFor="memberhistorical-date-start-end" 
-                                       style={{paddingTop:10}}>
-                                            Date début / fin
-                                </label>
-                                <div className="col-sm-2 memberhistorical" data-eusko="memberhistorical-date-start">
-                                    <DatePicker
-                                        name="dateSelectorBegin"
-                                        className="form-control"
-                                        selected={moment(this.state.beginDate)}
-                                        onChange={this.beginDateChange}
-                                        showYearDropdown 
-                                        locale="fr"
-                                    />
-                                </div>
-                                <div className="col-sm-2 memberhistorical" data-eusko="memberhistorical-date-end">
-                                    <DatePicker
-                                        name="dateSelectorEnd"
-                                        className="form-control"
-                                        selected={moment(this.state.endDate)}
-                                        onChange={this.endDateChange}
-                                        showYearDropdown 
-                                        locale="fr"
-                                    />
-                                </div>
-                                <div className="col-sm-3"></div>
                             </div>
                         </div>
-                    </div>
+                    </HistoricalForm>
                 </div>
                 <div className="form-group row no-bottom-space">
                 <div className="col-md-2 col-md-offset-9">

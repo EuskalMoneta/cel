7790cbefbd933114a1f742fa0b73ae57077e41e1
Florian Le Frioux <florian.lefrioux@meta-it.fr>
2017-03-31 14:45:48 +0200

    [FIX] #865 - Sous IE, même souci que précédemment mais avec la méthode .startsWith des objets String

--- src/cel/static/js/utils.jsx
+++ src/cel/static/js/utils.jsx
@@ -1,3 +1,10 @@
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function (searchString, position) {
+        position = position || 0;
+        return this.substr(position, searchString.length) === searchString;
+    };
+}
+
 if (!String.prototype.endsWith) {
     String.prototype.endsWith = function(searchString, position) {
         var subjectString = this.toString();

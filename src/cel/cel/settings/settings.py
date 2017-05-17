678b79be97fbc6afda73950b631df98d3954b247
Florian Le Frioux <florian.lefrioux@meta-it.fr>
2017-05-17 14:24:12 +0200

    [ADD] Add a slideshow in the login page.
    Customize the template of Django Hero Slider and add the slideshow in the template of the login page.

--- src/cel/cel/settings/settings.py
+++ src/cel/cel/settings/settings.py
@@ -183,5 +183,5 @@ STATICFILES_DIRS = (
     os.path.join(os.path.dirname(BASE_DIR), 'static'),
 )
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = './media/'
---
+++ src/cel/static/js/launch-carousel.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	// put all your jQuery goodness in here.
+ 	$('.carousel').carousel({
+    	/* interval: 3000 */
+    });
+});

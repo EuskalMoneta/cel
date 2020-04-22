0145ade91d0c643e54e340dc1d299c837cf1b6a6
Clément <contact@glukose.fr>
2020-04-22 16:57:10 +0200

    add: etape 3 visuels

--- symfony/src/Controller/APIToolbox.php
+++ symfony/src/Controller/APIToolbox.php
@@ -227,15 +227,20 @@ class APIToolbox extends AbstractController
     {
         $analysisResult = json_decode($analysisResult);
 
+        if($analysisResult->documentClassification->idType != 'ID'){
+            return ['status' => false, 'message' => 'Mauvais document'];
+        }
         foreach ($analysisResult->checkReportSummary->check as $check){
             if($check->identifier == 'SUMMARY_ID_IDENTIFIED' and $check->result != 'OK')
-                return false;
+                return ['status' => false, 'message' => $check->resultMsg];
+            if($check->identifier == 'SUMMARY_ID_IDENTIFIED' and $check->result != 'OK')
+                return ['status' => false, 'message' => $check->resultMsg];
             elseif ($check->identifier == 'SUMMARY_ID_FALSIFIED' and $check->result != 'OK')
-                return false;
+                return ['status' => false, 'message' => $check->resultMsg];
             elseif ($check->identifier == 'SUMMARY_ID_SPECIMEN' and $check->result != 'OK')
-                return false;
+                return ['status' => false, 'message' => $check->resultMsg];
             elseif ($check->identifier == 'SUMMARY_ID_COPY' and $check->result != 'OK')
-                return false;
+                return ['status' => false, 'message' => $check->resultMsg];
         }
         return true;
     }

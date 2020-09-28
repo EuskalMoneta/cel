c9f7e0b8047d5c6e8f4de7d305236663b457b081
Clément <contact@glukose.fr>
2020-09-28 14:57:16 +0200

    fix: typo env for yousign mode

--- symfony/src/Controller/OuvertureCompteController.php
+++ symfony/src/Controller/OuvertureCompteController.php
@@ -306,7 +306,7 @@ class OuvertureCompteController extends AbstractController
         //récupérer le SEPA signé et le stocker en session
         $webHook = $em->getRepository("App:WebHookEvent")->find($session->get('idWebHookEvent'));
 
-        $youSignClient = new WiziSignClient($_ENV['YOUSIGN_API_KEY'], $_ENV['YOUSIGN_API_KEY']);
+        $youSignClient = new WiziSignClient($_ENV['YOUSIGN_API_KEY'], $_ENV['YOUSIGN_MODE']);
         $file = $youSignClient->downloadSignedFile($webHook->getFile(), 'base64');
 
         $data = array_merge($session->get('utilisateur'), ['sepa_document' => $file]);
